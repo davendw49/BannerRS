dbc2d07d9467d9eda186973445c0cd6507cd44b8
qianyuning <75076732+qianyuning@users.noreply.github.com>
2021-11-22 17:35:24 +0800

    Add files via upload

---
+++ demo html/data.js
@@ -0,0 +1,72 @@
+var categories = [
+    
+    {
+ name: '品牌',
+ count: 0,
+ children: [{
+  name: '联想',
+ }, {
+  name: '小米',
+ 
+ }, {
+  name: '苹果',
+ 
+ }, {
+  name: '东芝',
+ 
+ }]
+}, 
+
+{
+ name: 'CPU',
+ count: 0,
+ children: [{
+  name: 'intel i7 8700K',
+ 
+ }, {
+  name: 'intel i7 7700K',
+ 
+ }, {
+  name: 'intel i9 9270K',
+ 
+ }, {
+  name: 'intel i7 8700',
+ 
+ }, {
+  name: 'AMD 1600X',
+ 
+ 
+ }]
+}, {
+ name: '内存',
+ count: 0,
+ children: [{
+  name: '七彩虹8G',
+ 
+ }, {
+  name: '七彩虹16G',
+ 
+ }, {
+  name: '金士顿8G',
+ 
+ }, {
+  name: '金士顿16G',
+ 
+ }]
+}, {
+ name: '显卡',
+ count: 0,
+ children: [{
+  name: 'NVIDIA 1060 8G',
+ 
+ }, {
+  name: 'NVIDIA 1080Ti 16G',
+ 
+ }, {
+  name: 'NVIDIA 1080 8G',
+ 
+ }, {
+  name: 'NVIDIA 1060Ti 16G',
+ 
+ }]
+}]
